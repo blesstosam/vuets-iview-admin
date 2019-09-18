88c2d0038040976c62b3d5b4be7adcd737331de1
blesstosam <blesstosam@126.com>
2019-09-18 21:18:50 +0800

    feat: polish new ui of NumberCard.vue

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,14 +3,21 @@
     <Row>
       <Col :xs="24" :sm="12" :md="6" v-for="(item, index) in totalData" :key="index">
         <NumberCard
-          :startVal="item.startVal"
           :endVal="item.targetValue"
-          :index="index"
-          :cardIndex="(index % 4) + 1"
-          :title="item.targetName"
           :subfix="item.targetCell"
+          :targetName="item.targetName"
           :explanation="item.explanation"
-        />
+          :icon="item.icon"
+        >
+          <div v-if="item.growth" style="color: #19be6b; font-weight: 500">
+            <Icon type="ios-arrow-up" />
+            <span style="vertical-align: middle; margin-left: 2px;">{{ item.growth }}%</span>
+          </div>
+          <div v-else style="color: #ed4014; font-weight: 500">
+            <Icon type="ios-arrow-down" />
+            <span style="vertical-align: middle; margin-left: 2px;">{{ item.reduce }}%</span>
+          </div>
+        </NumberCard>
       </Col>
     </Row>
   </div>
@@ -18,17 +25,49 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import NumberCard from '@/components/NumberCard.vue';
+import NumberCard from './NumberCard.vue';
 
 @Component({
   components: { NumberCard }
 })
 export default class Home extends Vue {
   totalData: Array<any> = [
-    { startVal: 0, targetValue: 100, targetName: '新增用户', targetCell: '人', explanation: '所有平台中新增用户' },
-    { startVal: 0, targetValue: 2000, targetName: '总用户', targetCell: '元', explanation: '所有平台中总用户' },
-    { startVal: 0, targetValue: 30000, targetName: '总消费额', targetCell: '元', explanation: '所有平台中总消费额' },
-    { startVal: 0, targetValue: 400, targetName: '人均消费额', targetCell: '元', explanation: '所有平台中人均消费额' }
+    {
+      startVal: 0,
+      targetValue: 320,
+      targetName: '新增用户',
+      targetCell: '人',
+      explanation: '所有平台中新增用户',
+      icon: 'ios-people',
+      growth: 110
+    },
+    {
+      startVal: 0,
+      targetValue: 2020,
+      targetName: '总用户',
+      targetCell: '人',
+      explanation: '所有平台中总用户',
+      icon: 'ios-people',
+      growth: 20
+    },
+    {
+      startVal: 0,
+      targetValue: 310000,
+      targetName: '总消费额',
+      targetCell: '元',
+      explanation: '所有平台中总消费额',
+      icon: 'logo-bitcoin',
+      reduce: 20
+    },
+    {
+      startVal: 0,
+      targetValue: 6060,
+      targetName: '人均消费额',
+      targetCell: '元',
+      explanation: '所有平台中人均消费额',
+      icon: 'logo-bitcoin',
+      growth: 10
+    }
   ];
 }
 </script>
---
+++ src/views/home/NumberCard.vue
@@ -0,0 +1,68 @@
+<style lang="stylus">
+.number-card
+  .ivu-card
+    margin-right 15px
+    .ivu-card-body
+      position relative
+      div
+        text-align center
+        .icon-wrap
+          background-color: rgb(230, 247, 255);
+          display: inline-block;
+          padding: 5px;
+          border-radius: 30px
+      & .ivu-tooltip
+        position absolute
+        right 10px
+        top: 10px
+      & .target-name
+        color #808695
+        font-size 13px
+        margin-bottom 8px
+    .value
+      font-size 28px
+      font-weight 700
+      margin 8px 0
+</style>
+
+<template>
+  <div class="number-card">
+    <Card dis-hover :bordered="false">
+      <Tooltip :content="explanation">
+        <div class="question-icon"><Icon color="#ccc" custom="iconfont icon-icon-circle-question" /></div>
+      </Tooltip>
+      <div>
+        <span class="icon-wrap">
+          <Icon :type="icon" color="rgb(64, 169, 255)" size="20" />
+        </span>
+      </div>
+      <div class="value">
+        <countTo :startVal="startVal" :endVal="endVal" :duration="2000" />
+        {{ subfix }}
+      </div>
+      <div class="target-name">{{ targetName }}</div>
+      <div><slot></slot></div>
+    </Card>
+  </div>
+</template>
+
+<script lang="ts">
+import countTo from 'vue-count-to';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({
+  components: { countTo }
+})
+export default class NumberCard extends Vue {
+  get tooLong() {
+    return this.endVal.toString().length >= 8;
+  }
+
+  @Prop({ default: 0 }) readonly startVal!: number;
+  @Prop({ default: 0 }) readonly endVal!: number;
+  @Prop({ default: '' }) readonly targetName!: string;
+  @Prop({ default: '' }) readonly subfix!: string;
+  @Prop({ default: '' }) readonly explanation!: string;
+  @Prop({ required: true }) readonly icon!: string;
+}
+</script>
