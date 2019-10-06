d7069cffef82bd7079d1534f16862c00daf35c0b
blesstosam <blesstosam@126.com>
2019-10-06 11:10:00 +0800

    feat: update chat page

--- src/views/chat/Chat.vue
+++ src/views/chat/Chat.vue
@@ -37,7 +37,6 @@
       display flex
       justify-content space-between
       padding 8px
-      border-bottom 1px solid #ccc
       .current-avatar
         span
           font-size 18px
@@ -52,14 +51,68 @@
         margin-right 5px
         margin-top 5px
     .content
-      border-bottom 1px solid #ccc
+      background #f3f4f7
+      padding-top: 8px
+      padding-bottom 24px
       height calc(100% - 157px)
+      overflow-y auto
+      .row
+        padding: 0 15px;
+        margin-top: 20px;
+        display: flex;
+        .avatar
+          width: 35px;
+          height: 35px;
+          display: block;
+        & .text
+          text-align: left;
+          margin-top: 0;
+          background: #fff;
+          padding: 8px;
+          box-sizing: border-box;
+          position: relative;
+          border-radius: 4px;
+          max-width: 80%;
+          font-size: 14px;
+        &-left
+          .text
+            margin-left: 12px;
+          & .text:before
+            width: 0;
+            height: 0;
+            position: absolute;
+            left: -12px;
+            top: 11px;
+            content: "";
+            border: 6px solid transparent;
+            border-right-color: #fff
+        &-right
+          flex-direction: row-reverse;
+          .text
+            margin-right: 12px;
+          & .text:after
+            width: 0;
+            height: 0;
+            position: absolute;
+            right: -12px;
+            top: 11px;
+            content: "";
+            border: 6px solid transparent;
+            border-left-color: #fff
+    .input-box
+      height 100px
+      .ivu-input
+        height 100px
+        border 0
+        outline none
+        &.ivu-input:focus
+          box-shadow none
 </style>
 
 <template>
   <div class="chat-page">
     <div class="chat-list">
-      <Input search placeholder="Enter something..." style="margin-bottom: 24px; margin-top: 3px;" />
+      <Input search style="margin-bottom: 24px; margin-top: 3px;" />
       <li class="chat-list-item" v-for="(item, index) in chatList" :key="index" @click="currentChatIndex = index">
         <img :src="item.avatar" alt="avatar" class="avatar" />
         <div class="introduction">
@@ -76,8 +129,22 @@
         </div>
         <div><Icon type="md-add" size="24" /></div>
       </div>
-      <div class="content">111</div>
-      <div class="input-box">footer</div>
+
+      <div class="content">
+        <div
+          class="row"
+          :class="[item.type === 'other' ? 'row-left' : 'row-right']"
+          v-for="(item, index) in currentChatList"
+          :key="'index' + index"
+        >
+          <img class="avatar" :src="item.type === 'other' ? currentChatItem.avatar : myAvatar" alt="avatar" />
+          <p class="text">{{ item.msg }}</p>
+        </div>
+      </div>
+
+      <div class="input-box">
+        <Input type="text" />
+      </div>
     </div>
   </div>
 </template>
@@ -89,6 +156,7 @@ interface ChatList {
   avatar: string;
   name: string;
   latestMsg: string;
+  msgList: Array<{ msg: string; type: string }>;
 }
 
 @Component({
@@ -102,22 +170,57 @@ export default class Chat extends Vue {
     return this.chatList[this.currentChatIndex];
   }
 
+  get currentChatList() {
+    return this.chatList[this.currentChatIndex].msgList;
+  }
+
   chatList: Array<ChatList> = [
     {
       avatar: require('@/assets/img/avatar.png'),
       name: 'Blesstosam',
-      latestMsg: 'hello, I am Sam'
+      latestMsg: 'hello, I am Sam',
+      msgList: [
+        {
+          msg: 'hello, I am Sam',
+          type: 'other'
+        },
+        { msg: '哈哈哈', type: 'me' },
+        { msg: '嘻嘻嘻', type: 'other' },
+        { msg: '嘻嘻嘻', type: 'other' },
+        { msg: '嘻嘻嘻', type: 'other' },
+        { msg: '嘻嘻嘻', type: 'other' },
+        { msg: '嘻嘻嘻', type: 'other' },
+        { msg: '嘻嘻嘻', type: 'other' }
+      ]
     },
     {
       avatar: require('@/assets/img/avatar1.jpeg'),
       name: 'Kal',
-      latestMsg: 'hello, I am Kal'
+      latestMsg: 'hello, I am Kal',
+      msgList: [
+        {
+          msg: 'hello, I am Kal',
+          type: 'other'
+        },
+        { msg: '哈哈哈', type: 'me' },
+        { msg: '嘻嘻嘻', type: 'other' }
+      ]
     },
     {
       avatar: require('@/assets/img/avatar2.jpeg'),
       name: 'Jason',
-      latestMsg: 'hello, I am Jason'
+      latestMsg: 'hello, I am Jason',
+      msgList: [
+        {
+          msg: 'hello, I am Jason',
+          type: 'other'
+        },
+        { msg: '哈哈哈', type: 'me' },
+        { msg: '嘻嘻嘻', type: 'other' }
+      ]
     }
   ];
+
+  myAvatar = 'https://avatar-static.segmentfault.com/285/486/285486960-59226f974c266_big64';
 }
 </script>
