a7daf3725decd924b408bba3c9d4e26baf9a2a88
blesstosam <blesstosam@126.com>
2019-10-31 10:44:58 +0800

    feat: add i18n in commoncardtitle

--- src/components/CommonCardTitle.vue
+++ src/components/CommonCardTitle.vue
@@ -2,9 +2,9 @@
   <div class="common-card-title" :class="`card-title-${headerTheme}`">
     <div>
       <Icon :size="16" :type="$route.meta.icon" style="position: relative; top: -1px;" />
-      <span> {{ $t($route.meta.title) }} </span>
+      <span> {{ isI18nTitle ? $t($route.meta.title) : $route.meta.title }} </span>
       <div style="font-size: 13px; font-weight: normal;">
-        {{ $t($route.meta.remark) }}
+        {{ isI18nRemark ? $t($route.meta.remark) : $route.meta.remark }}
       </div>
     </div>
     <div class="action-wrap" style="margin-top: 12px;">
@@ -21,5 +21,13 @@ import { HeaderThemeType } from '@/store/module/setting';
 @Component({})
 export default class CommonCardTitle extends Vue {
   @Getter('headerTheme') headerTheme!: HeaderThemeType;
+
+  get isI18nTitle() {
+    return this.$route.meta.title.indexOf('menutitle') !== -1;
+  }
+
+  get isI18nRemark() {
+    return this.$route.meta.remark.indexOf('remark') !== -1;
+  }
 }
 </script>
