d9a526f0d73bfc365ebae3970617ac4dc050801e
blesstosam <blesstosam@126.com>
2019-12-12 13:09:22 +0800

    feat: 增加打包环境

--- src/api/server.ts
+++ src/api/server.ts
@@ -3,8 +3,15 @@ let baseUrl = '',
 
 switch (process.env.NODE_ENV) {
   case 'production':
-    baseUrl = '';
-    wsUrl = '';
+    // 预上线 - 测试环境
+    if (process.env.VUE_APP_CURRENTMODE === 'predeploy') {
+      baseUrl = '';
+      wsUrl = '';
+    } else {
+      // 正式生产环境
+      baseUrl = '';
+      wsUrl = '';
+    }
     break;
   case 'development':
     baseUrl = '';
