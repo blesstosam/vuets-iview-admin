1c3d16c6065494476e669058a0237276ec5ee48b
blesstosam <blesstosam@126.com>
2019-12-18 17:54:10 +0800

    fix: add test:ci command

--- .eslintrc.js
+++ .eslintrc.js
@@ -124,6 +124,10 @@ module.exports = {
      * 禁止对已定义的 class 重新赋值
      */
     'no-class-assign': 'error',
+    /**
+     * 必须使用 ... 而不是 Object.assign，除非 Object.assign 的第一个参数是一个变量
+     */
+    'prefer-object-spread': 'error',
 
 
     /************************ ts相关 目前不增加过多相关检查 *****************************/
