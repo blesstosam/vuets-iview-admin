0123e1e1f4b2185fac9bb5f4b951a47d6597f827
blesstosam <blesstosam@126.com>
2020-02-15 17:42:20 +0800

    refactor 添加删除按钮组件

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <Poptip v-on="$listeners" v-bind="$attrs" :placement="placement" confirm :title="confirmMsg">
+    <Button type="error" size="small">删除</Button>
+  </Poptip>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class DeleteButton extends Vue {
+  @Prop({ default: '确定要删除吗？' }) readonly confirmMsg!: string;
+  @Prop({ default: 'left-end' }) readonly placement!: string;
+}
+</script>
